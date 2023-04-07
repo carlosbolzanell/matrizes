89a1e6d345bd0a4bcdc0351d2e9245dda9b4eaa1
carlosbolzanell <carlosbolzanell@gmail.com>
2023-04-06 21:31:39 -0300

    Sudoku

---
+++ Sudoku/src/pratica.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+public class pratica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int jogadores;
+		String nomes;
+		
+		System.out.println("                              ***REGRAS***                                   ");
+		System.out.println("Não pode repetir os mesmos números nas mesmas linhas, colunas e casinhas(3x3)");
+		
+		System.out.println("\nQuantos jogadores vão jogar?");
+		jogadores = sc.nextInt();
+		
+		for(int jogadas=0; jogadas<jogadores; jogadas++) {
+			System.out.println("Qual o seu nome?");
+			nomes = sc.next();
+		}
+		int [][] sudoku = new int [9][9];
+		System.out.println("=========SUDOKU==========");
+		for(int linha=0; linha<sudoku.length; linha++) {
+			for(int coluna=0; coluna<sudoku[0].length; coluna++) {
+				sudoku[linha][coluna] = 0;
+				if(coluna==0 || coluna==3 || coluna==6) {
+					System.out.print("| "+sudoku[linha][coluna]+" ");
+				}else {
+					System.out.print(sudoku[linha][coluna]+" ");
+				}
+				
+			}
+			System.out.print("|\n");
+			if(linha==2 || linha==5 || linha==8) {
+				System.out.print("-------------------------");
+			System.out.print("\n");
+	
+		}
+		
+	}
+		for(int contador=0; contador<81; contador++) {
+			
+			int escolha, linhas, colunas;
+			System.out.println("Qual numero?");
+			escolha = sc.nextInt();
+			System.out.println("Qual linha?");
+			linhas = sc.nextInt();
+			System.out.println("Qual coluna?");
+			colunas = sc.nextInt();
+			
+			System.out.println("=========SUDOKU==========");
+			for(int linha=0; linha<sudoku.length; linha++) {
+				for(int coluna=0; coluna<sudoku[0].length; coluna++) {
+					
+					sudoku[linhas][colunas]	=escolha;
+					
+					if(coluna==0 || coluna==3 || coluna==6) {
+						System.out.print("| "+sudoku[linha][coluna]+" ");
+					}else {
+						System.out.print(sudoku[linha][coluna]+" ");
+					}
+				
+					
+				}
+				System.out.print("|\n");
+				if(linha==2 || linha==5 || linha==8) {
+					System.out.print("-------------------------");
+				System.out.print("\n");
+
+
+					}
+				
+
+				}
+			}
+			
+		}
+	}
+
+
