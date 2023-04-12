ee9df0a79532f73d7458720a257cb071998129bc
carlosbolzanell <carlosbolzanell@gmail.com>
2023-04-12 18:25:37 -0300

    Update ex5.java

--- matriz4/src/matriz4/ex5.java
+++ matriz4/src/matriz4/ex5.java
@@ -8,25 +8,24 @@ public class ex5 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
-		int numero, aux;
+		int numero, aux=1;
 		System.out.println("Numero");
 		numero = sc.nextInt();
 		
 		int [][] matriz = new int [numero][numero];
 		
 		for(int linha = 0; linha<matriz.length; linha++) {
-			for(int coluna=0; coluna<matriz.length; coluna++) {
-				if (linha+coluna != matriz.length-1) {
-					System.out.print(" ");
+			for(int coluna=0; coluna<numero; coluna++) {
+				if(coluna==linha) {
+					matriz[linha][coluna] = coluna+1;	
+					System.out.print(matriz[linha][coluna]+ " ");
 				}else {
-					for(int i=0; i< coluna+1; i++) {
-						System.out.print(coluna+1);
-					}
-					System.out.print("*");
+					matriz[linha][coluna] = " ";
 				}
-
+		
 			}
 			System.out.print("\n");
+			
 
 		}	
 	}
